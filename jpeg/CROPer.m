2ab6af2d1b565ce1d9095dcd78af53e18c8ad526
Michael Omori <omorim@uw.edu>
2016-09-07 11:26:49 -0700

    Add files via upload

--- jpeg/CROPer.m
+++ jpeg/CROPer.m
@@ -1,70 +1,27 @@
-% This programs does an initial crop of an image and saves that data
+% Michael Omori
+% Summer 2016
 
-imagefiles = dir('**.JPG'); 
+% This program sets up the automatic cropping for crop.m
+
+% loads the jpg images
+imagefiles = dir('**.JPG');
+nfiles = length(imagefiles); % Number of files found
+material_names = cell(1,nfiles);
 image = imread(imagefiles(1).name);
-t = 1;
-cm = msgbox('select region and double clip to crop');
-pause(t);
-a = msgbox('crop the white');
-pause(t);
-[white_image, rect_white] = imcrop(image);
-delete(cm)
-delete(a)
-b = msgbox('crop the black');
-pause(t);
-[black_image, rect_black] = imcrop(image);
-delete(b)
-c = msgbox('crop the material');
-pause(t);
-[material_image, rect_material] = imcrop(image);
-delete(c)
-d = msgbox('crop the gray');
-pause(t);
-[gray_image, rect_gray] = imcrop(image);
-delete(d);
+crop_boundaries = zeros(num_references+1, 4);
 
-d = msgbox('crop the one');
-pause(t);
-[one_image, rect_one] = imcrop(image);
-delete(d);
-d = msgbox('crop the two');
-pause(t);
-[two_image, rect_two] = imcrop(image);
-delete(d);
-d = msgbox('crop the three');
-pause(t);
-[three_image, rect_three] = imcrop(image);
-delete(d);
-d = msgbox('crop the four');
-pause(t);
-[four_image, rect_four] = imcrop(image);
-delete(d);
-d = msgbox('crop the five');
-pause(t);
-[five_image, rect_five] = imcrop(image);
-delete(d);
-d = msgbox('crop the six');
-pause(t);
-[six_image, rect_six] = imcrop(image);
-delete(d);
-d = msgbox('crop the seven');
-pause(t);
-[seven_image, rect_seven] = imcrop(image);
-delete(d);
-d = msgbox('crop the eight');
-pause(t);
-[eight_image, rect_eight] = imcrop(image);
-delete(d);
-d = msgbox('crop the nine');
-pause(t);
-[nine_image, rect_nine] = imcrop(image);
-delete(d);
-d = msgbox('crop the ten');
-pause(t);
-[ten_image, rect_ten] = imcrop(image);
-delete(d);
+% obtains the crop boundaries for each reference card and material
+for i=1:num_references+1
+   if i == num_references+1
+       ms = msgbox('select material to crop');
+   else
+       ms = msgbox(sprintf('select reference %d and double clip to crop', i));
+   end
+   pause(1);
+   [~, rect] = imcrop(image);
+   crop_boundaries(i,:) = rect;
+   delete(ms);
+end
 
-save('rect_data.mat');
 close all;
-
-'finished CROPer'
+'finished crop setup'
